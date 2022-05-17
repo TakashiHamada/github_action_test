4d27b0baf4a6c911f598a90eef2c70b80eb1a366
TakashiHamada <hamada@gift10.net>
2022-05-17 12:14:43 +0900

    key eventの取得

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var app = new Vue({
     el: '#app',
     data: {
-        test: "",
+        key: "",
+        keyCode: null
     },
     created: function () {
     },
+    mounted() {
+        document.addEventListener('keydown', this.onKeyDown)
+    },
+    beforeDestroy() {
+        document.removeEventListener('keydown', this.onKeyDown)
+    },
     methods: {
+        onKeyDown(event) {
+            this.key = event.key
+            this.keyCode = event.keyCode
+        }
     }
 })
 
